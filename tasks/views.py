68d74bc8d18509667873dcbc9f27f270359eb5f2
Rob Serowka <robert.serowka@hotmail.co.uk>
2020-11-08 11:10:23 +0000

    Task Queries Added

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+# REST Framework Imports
+from rest_framework import status
+from rest_framework.exceptions import NotFound
+from rest_framework import viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+# Model and serializer Imports
+from .models import Task
+from .serializers import TaskSerializer
+
+
+#  ----------------------------------------------------------------------------
+#                                   TASK QUERIES
+#  ----------------------------------------------------------------------------
+#  I decided to use REST framework exception 'NotFound()' rather than Django's 'get_object_or_404' 
+#  just for bit of consistency despite slightly more lines of code
+def get_all_tasks():
+    try:
+      return Task.objects.all()
+    except Task.DoesNotExist:
+      raise NotFound(detail='Tasks could not be found, please try a different request')
+    
+def get_task(pk):
+    try:
+      return Task.objects.get(pk=pk)
+    except Task.DoesNotExist:
+      raise NotFound(detail='Task does not appear to exist, please try a different request')
+    
