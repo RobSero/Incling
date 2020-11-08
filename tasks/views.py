084b55530590e0bdc1c83a0ba00bd32326e8ea01
Rob Serowka <robert.serowka@hotmail.co.uk>
2020-11-08 11:19:06 +0000

    Task ViewSet Added

--- tasks/views.py
+++ tasks/views.py
@@ -25,4 +25,46 @@ def get_task(pk):
       return Task.objects.get(pk=pk)
     except Task.DoesNotExist:
       raise NotFound(detail='Task does not appear to exist, please try a different request')
-    
+
+
+
+#  ----------------------------------------------------------------------------
+#                                  TASK VIEWSETS
+#  ----------------------------------------------------------------------------
+
+
+class TaskViewSet(viewsets.ViewSet):
+  """  Contins the logic for handling the Task CRUD functionality """
+  
+  def list(self,request):
+    qs_all_tasks = get_all_tasks()
+    serialized_tasks = TaskSerializer(qs_all_tasks, many=True)
+    return Response(serialized_tasks.data, status=status.HTTP_200_OK)
+  
+  
+  def create(self, request):
+    new_task = TaskSerializer(data=request.data)
+    if new_task.is_valid():
+      new_task.save()
+      return Response(new_task.data, status=status.HTTP_201_CREATED)
+    return Response(new_task.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+  
+  
+  def retrieve(self, request, pk=None):
+    qs_task = get_task(pk=pk)
+    serialized_task = TaskSerializer(qs_task)
+    return Response(serialized_task.data, status=status.HTTP_200_OK)
+  
+  
+  def update(self,request, pk=None):
+    pass
+  
+  
+  def partial_update(self,request, pk=None):
+    pass
+  
+  
+  def destroy(self,request, pk=None):
+    qs_task = get_task(pk=pk)
+    qs_task.delete()
+    return Response({'message': 'deleted successfully'}, status=status.HTTP_202_ACCEPTED)
