c873d1031360210a10fc03f83d90c35724899ec6
Rob Serowka <robert.serowka@hotmail.co.uk>
2020-11-08 11:26:33 +0000

    Small Test Suite Added

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.db.models import Q
 
-# Create your tests here.
+# Model and serializer Imports
+from .models import Task
+
+
+class TaskTestSuite(TestCase):
+  """  Handles the tests for creating tasks and querying the database  """
+  def setUp(self):
+    Task.objects.create(title='Clear dashboard', description='it is looking pretty messy dude', task_type='2')
+    Task.objects.create(title='Get Cheap Pens', description='you know they are gonna die within a day, buy a decent one...', task_type='0')
+    Task.objects.create(title='Delegate new projects', description='get this done ASAP before next quarter', task_type='3')
+
+    
+    
+  def test_get_task(self):
+    obj_task = Task.objects.get(pk=1)
+    self.assertTrue(obj_task)
+    self.assertEqual(obj_task.title, 'Clear dashboard')
+
+    
+  def test_search_queries(self):
+    qs_search = Task.objects.filter(Q(title__icontains='cheap') | Q(description__icontains='messy') )
+    self.assertTrue(qs_search)
+    self.assertEqual(len(qs_search), 2)
+    self.assertEqual(qs_search.last().title, 'Get Cheap Pens')
+
+  
+
+
