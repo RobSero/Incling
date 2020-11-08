6f724e10f4e0fb2a5528a07f95375c358c59373e
Rob Serowka <robert.serowka@hotmail.co.uk>
2020-11-08 11:52:47 +0000

    Router urls setup

--- incling/urls.py
+++ incling/urls.py
@@ -1,14 +1,21 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 # REST Framework Imports
 from rest_framework.routers import DefaultRouter
 
 # View Imports
+from tasks.views import TaskViewSet
+from tiles.views import TileViewSet
 
 
 router = DefaultRouter()
+router.register('tiles', TileViewSet, basename='tile-viewset')
+router.register('tasks', TaskViewSet, basename='task-viewset')
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
