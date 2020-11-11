86cf1f35f496273d9b09ed5f8d020d46d58f5c62
Rob Serowka <robert.serowka@hotmail.co.uk>
2020-11-11 19:08:35 +0000

    navbar icons and styling

---
+++ frontend/src/components/common/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+import HomeIcon from '@material-ui/icons/Home';
+import AirplanemodeActiveIcon from '@material-ui/icons/AirplanemodeActive';
+import AppleIcon from '@material-ui/icons/Apple';
+import AssignmentIcon from '@material-ui/icons/Assignment';
+
+function Navbar() {
+  return (
+    <nav>
+      <div className='user-details'>
+       <Link to='/'><HomeIcon className='nav-item' /></Link>
+        <AirplanemodeActiveIcon className='nav-item' />
+      </div>
+      <div className='search-fields'>
+      <AppleIcon className='nav-item' />
+        <AssignmentIcon className='nav-item' />
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
